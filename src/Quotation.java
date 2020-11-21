ba76f1e3d7198341305efd14d14ee48f854339d2
scarlens250 <74828654+scarlens250@users.noreply.github.com>
2020-11-21 20:35:43 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,6 @@
+public class Quotation {
+  String quote = "Age is whatever you think it is. You are as old as you think you are.	";
+  public void display() {
+    System.out.println(quote);
+  }
+}
